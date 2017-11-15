55c0500795c79936dd941014299b704718d61749
jonesrandom <jones.random97@gmail.com>
2017-11-15 11:53:34 +0800

    use annotations for Parcelable

--- app/src/main/java/com/jonesrandom/sqlite_kotlin/model/ModelMahasiswa.kt
+++ app/src/main/java/com/jonesrandom/sqlite_kotlin/model/ModelMahasiswa.kt
@@ -1,7 +1,9 @@
 package com.jonesrandom.sqlite_kotlin.model
 
-import android.os.Parcel
+
+import android.annotation.SuppressLint
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by jonesrandom on 11/14/17.
@@ -9,36 +11,10 @@ import android.os.Parcelable
  * #JanganLupaBahagia
  *
  */
-class ModelMahasiswa(var id: Int, var nama: String, var nim: Int, var semster: String) : Parcelable {
 
-
-    constructor(parcel: Parcel) : this(
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readInt(),
-            parcel.readString())
-
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class ModelMahasiswa(var id: Int, var nama: String, var nim: Int, var semster: String) : Parcelable {
     constructor() : this(0, "", 0, "")
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(nama)
-        parcel.writeInt(nim)
-        parcel.writeString(semster)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<ModelMahasiswa> {
-        override fun createFromParcel(parcel: Parcel): ModelMahasiswa {
-            return ModelMahasiswa(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ModelMahasiswa?> {
-            return arrayOfNulls(size)
-        }
-    }
 
 }
